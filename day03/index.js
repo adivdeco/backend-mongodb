3dba75e5ddfea913e9ddb72ab2d926c316a7e677
adivdeco <sadiv120@gmail.com>
2025-04-10 22:44:04 +0530

    authonticatiomn

--- day03/index.js
+++ day03/index.js
@@ -5,9 +5,11 @@ const main = require('./database');
 const User = require('./module/userSchemas');
 const validateUser = require('./utils/validator');
 const bcrypt = require('bcrypt');
+const cookieParser = require('cookie-parser') // to parse the cookies from the request
+const jwt = require('jsonwebtoken');
 
 app.use(express.json()) // to parse the json data from the request body
-
+app.use(cookieParser()) // to parse the cookies from the request
 
 const spark = new User ({name:"hell and clean"})   // this is just to check if the model is working or not
 console.log(spark.name)
@@ -33,29 +35,52 @@ app.post('/register',async (req, res) => {
 
 app.post('/login',async (req, res) => {
     try{
-       const people = await User.findById(req.body._id)
-        
-       const validemail = (req.body.email===people.email)
-        if(!validemail){
-            return res.status(400).send({message:"invaalid Crentials"})
+    //    const people = await User.findById(req.body._id)
+    const people = await User.findOne({email:req.body.email})
+    // console.log(people.email);
+    
+        if(!people){
+            throw new Error("invasslid Crentials")
         }
+
+        // comparing passward
      const passMatch = await bcrypt.compare(req.body.passward ,people.passward)
         if(!passMatch){
              throw new Error("invalid Crentials")
         }
-       if (validemail , passMatch) {
-        console.log("login successfully")
-       } res.send({message:"login successfully",people})
-    }
-    catch(err){
+       
+
+        // jwt tocken
+
+        const tocken = jwt.sign({_id: people._id , email: people.email}, "adiv1234" , {expiresIn:100})
+        res.cookie("tocken",tocken)
+
+        res.send("login successfully")
+    }catch(err){
         res.send("Error "+ err.message)
     }
 }
 )
 
 app.get("/user" , async (req, res) => {
+   try{
     const data = await User.find({})
+   
+    const payload =jwt.verify(req.cookies.tocken,"adiv1234")
+    if(!payload){
+        return res.status(401).send({message:"unauthorized"})
+    }
+    console.log(payload);
+    
+   
+    // console.log(req.cookies);
     res.send({message:"hear is all data",data})
+}
+
+     catch(err){
+        res.send("Error"+ err.message)
+    }
+
 })
 
 app.get('/user/:id', async (req, res) => {
